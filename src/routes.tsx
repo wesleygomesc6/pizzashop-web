5efef3da155f476a3b75ec16208f591336b2fa4f
wesleygomes <wesleygomes@creago.org.br>
2024-10-15 11:45:20 -0300

    feat: alterar tema | pagina de pedidos

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/ortders/orders'
 import { SingIn } from './pages/auth/sign-in'
 import { SingUp } from './pages/auth/sign-up'
 
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
